3ef92265e2e3b747696893110d1bee20abcdfa26
Baskaran1995 <baskarkrish6@gmail.com>
2022-10-08 05:03:24 +0530

    first commit

---
+++ tagname.js
@@ -0,0 +1,18 @@
+var li = document.getElementByClassName('list-group-li');
+
+console.log(li);
+
+console.log(li [1])
+
+li[1].textContent = "Hello 2";
+
+li[2].style.fontweight = 'bold';
+
+li[3].style.backgroundColor= 'Green';
+
+for(var i=0; i<li.length; i++){
+
+li[i].style.backgroundColor= '#f4f4f4';
+
+}
+
